be7fbf5664b3ce722ad7471e24205a4a3b23eacd
Danny Suazo <dannysuazo@MacBook-Pro-2.hsd1.tx.comcast.net>
2020-03-13 13:43:22 -0500

    Updates

---
+++ precache-manifest.d4924a74979aba89e1c330c6e01f7c8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "006f4b2314cf9417dc646662130e256d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "727a6a181a92419f7aef",
+    "url": "/portfolio/static/css/main.f923ea8f.chunk.css"
+  },
+  {
+    "revision": "644992cf89e1fb95a8d6",
+    "url": "/portfolio/static/js/2.f8384d6e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.f8384d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "727a6a181a92419f7aef",
+    "url": "/portfolio/static/js/main.f83b95ef.chunk.js"
+  },
+  {
+    "revision": "8e749b8aed89b904cb19",
+    "url": "/portfolio/static/js/runtime-main.a22ba609.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
