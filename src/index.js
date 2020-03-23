55bf025991350891c97c7c7c9091418e8fa82d73
Danny Suazo <dannysuazo@MacBook-Pro-2.hsd1.tx.comcast.net>
2020-03-22 23:45:24 -0500

    added google analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-161628164-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
