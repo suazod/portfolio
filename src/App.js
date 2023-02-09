873fe11bb0e748bcf58538e17f410f496f247daf
Danny Suazo <dsuazo@opportune.com>
2023-02-09 00:03:01 -0600

    updated personal portfolio

--- src/App.js
+++ src/App.js
@@ -11,29 +11,30 @@ import Lsc from './pages/Lsc';
 import Hiretech from './pages/Hiretech';
 import Weather from './pages/Weather';
 
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 //const mainBg = { backgroundImage: 'url(' + require('./styles/images/main.png') + ')', backgroundRepeat: 'no-repeat', backgroundSize:'cover' };
 function App() {
   return (
-    <Router>
     <div className="App">
-        <Route path="/" component={Welcome} exact/>
-        <Route path="/About" component={About} />
-        <Route path="/Portfolio" component={Works} />
-        <Route path="/AudioBuzz" component={AudioBuzz} />
-        <Route path="/Petential" component={Petential} />
-        <Route path="/Bravoo" component={Bravoo} />
-        <Route path="/Halloween" component={Halloween} />
-        <Route path="/Karaoke" component={Karaoke} />
-        <Route path="/Lsc" component={Lsc} />
-        <Route path="/Hiretech" component={Hiretech} />
-        <Route path="/Weather" component={Weather} />
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/" element={<Welcome />} exact />
+          <Route path="/About" element={<About />} />
+          <Route path="/Portfolio" element={<Works />} />
+          <Route path="/AudioBuzz" element={<AudioBuzz />} />
+          <Route path="/Petential" element={<Petential />} />
+          <Route path="/Bravoo" element={<Bravoo />} />
+          <Route path="/Halloween" element={<Halloween />} />
+          <Route path="/Karaoke" element={<Karaoke />} />
+          <Route path="/Lsc" element={<Lsc />} />
+          <Route path="/Hiretech" element={<Hiretech />} />
+          <Route path="/Weather" element={<Weather />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
-    </Router>
   );
 }
 
